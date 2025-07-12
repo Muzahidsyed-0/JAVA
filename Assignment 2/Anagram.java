c313e99baa1727a10e47c0f483a2f42f8b8c1905
Muzahid Syed <muzahid.syed.official@gmail.com>
2025-07-12 11:58:45 +0530

    Add files via upload

---
+++ Assignment 2/Anagram.java
@@ -0,0 +1,47 @@
+package assignment;
+
+import java.util.Arrays;
+
+public class Anagram {
+	
+	public static boolean anagram(String str1, String str2) {
+		
+		str1=str1.replaceAll("\\s", "").toLowerCase();
+		
+		str2=str2.replaceAll("\\s", "").toLowerCase();
+		 
+		if(str1.length()!= str2.length()) {
+			
+			return false;	
+		}
+		
+		char[] charArray1= str1.toCharArray();
+		
+		char[] charArray2=str2.toCharArray();
+		
+		Arrays.sort(charArray1);
+		
+		Arrays.sort(charArray2);
+		
+		return Arrays.equals(charArray1,charArray2);
+	}
+
+	public static void main(String[] args) {
+		
+		String word1="listen";
+		
+		String word2="silent";
+		
+		if(anagram(word1,word2)) {
+			
+		System.out.println(word1  + "and" +  word2 +  "are anagrams");
+		
+		}
+		else {
+			
+			System.out.println(word1+"and"+ word2 +" are not aagrams");
+		}
+
+	}
+
+}
